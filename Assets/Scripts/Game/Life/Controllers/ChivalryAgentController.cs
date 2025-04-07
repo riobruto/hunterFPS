8a8fa88eeb8fedbcd42e1c351043d29e8e1203af
RioBruto <coppariobruto@gmail.com>
2025-04-07 13:44:35 -0300

    added horse npc

---
+++ Assets/Scripts/Game/Life/Controllers/ChivalryAgentController.cs
@@ -0,0 +1,454 @@
+﻿using System;
+using System.Linq;
+using Game.Entities;
+using Game.Life;
+using Life.Entities;
+using Life.StateMachines;
+using UnityEngine;
+
+namespace Life.Controllers
+{
+    public class ChivalryAgentController : AgentController
+    {
+        [Header("References")]
+        [SerializeField] private Transform _npcMount;
+
+        [SerializeField] public LayerMask CoverMask;
+        [SerializeField] private AgentFireWeapon _weapon;
+        [SerializeField] public Animator Soldier;
+
+        [Header("Combat")]
+        [SerializeField] private HorseNavHintGroup _combatHintsGroup;
+
+        [SerializeField] private LimbHitbox[] _humanHitboxes;
+        [SerializeField] private LimbHitbox[] _horseHitboxes;
+
+        private Vector3 _attackPoint;
+
+        public override void OnStart()
+        {
+            CanLoseContact = true;
+
+            SetSpeed(6);
+            SetMaxHealth(200);
+            SetHealth(200);
+            SetUpStates();
+            SetUpTransitions();
+            NavMeshAgent.updatePosition = false;
+            NavMeshAgent.updateRotation = true;
+            NavMeshAgent.angularSpeed = 120f;
+            Animator.applyRootMotion = true;
+            _weapon.AllowReload = true;
+
+            SetLimboxes();
+        }
+
+        private void SetLimboxes()
+        {
+            foreach (LimbHitbox hLimb in _humanHitboxes)
+            {
+                hLimb.LimbHitEvent += OnHumanLimbHurt;
+            }
+            foreach (LimbHitbox horseLimb in _horseHitboxes)
+            {
+                horseLimb.LimbHitEvent += OnLimbHurt;
+            }
+        }
+
+        private float _soldierHealth = 100;
+
+        private void OnHumanLimbHurt(LimboxHit payload)
+        {
+            if (_soldierHealth < 0) return;
+            if (payload.Hitbox.Type == LimbType.HEAD) _soldierHealth = 0;
+            else _soldierHealth -= payload.Damage;
+
+            if (_soldierHealth <= 0)
+            {
+                OnSoldierDeath();
+            }
+        }
+
+        private void OnSoldierDeath()
+        {
+            //ir al estado de despawn
+            Soldier.enabled = false;
+            Soldier.transform.SetParent(null);
+            _weapon.AllowFire = false;
+            foreach (LimbHitbox hLimb in _humanHitboxes)
+            {
+                hLimb.Ragdoll();
+            }
+        }
+
+        public override void OnLimbHurt(LimboxHit payload)
+        {
+            if (IsDead) return;
+            SetHealth(GetHealth() - payload.Damage);
+            if (GetHealth() <= 0)
+            {
+                OnDeath();
+            }
+        }
+
+        public override void OnDeath()
+        {
+            Animator.enabled = false;
+            foreach (LimbHitbox horseLimb in _horseHitboxes)
+            {
+                horseLimb.Ragdoll();
+            }
+            OnSoldierDeath();
+            _soldierHealth = 0;
+        }
+
+        private HorsePatrolActBusyState _horsePatrol;
+        private HorseApproachCombatState _approachCombat;
+        private HorseRetreatCombatState _retreatCombat;
+
+        private void SetUpStates()
+        {
+            _horsePatrol = new(this);
+            _approachCombat = new(this);
+            _retreatCombat = new(this);
+        }
+
+        private void SetUpTransitions()
+        {
+            Machine.AddAnyTransition(_horsePatrol, new FuncPredicate(() => false));
+            Machine.AddTransition(_horsePatrol, _approachCombat, new FuncPredicate(() => _shouldApproachCombat));
+
+            Machine.AddTransition(_approachCombat, _retreatCombat, new FuncPredicate(() => _approachCombat.Completed));
+            Machine.AddTransition(_retreatCombat, _approachCombat, new FuncPredicate(() => _retreatCombat.Completed));
+
+            Machine.SetState(_horsePatrol);
+        }
+
+        private bool _shouldApproachCombat { get => HasPlayerVisual; }
+        private bool _shouldRetreatCombat { get; }
+        private bool _shouldGetAlerted { get; }
+
+        private void OnAnimatorMove()
+        {
+            // if (!Initialized) return;
+
+            Vector3 root = Animator.rootPosition;
+            root.y = NavMeshAgent.nextPosition.y;
+
+            transform.position = root;
+            NavMeshAgent.nextPosition = root;
+        }
+
+        private Vector2 _smoothDeltaPos;
+        private Vector2 _velocity;
+        private float _maxspeed;
+
+        public Vector3 AttackPoint { get => _attackPoint; }
+        public HorseNavHintGroup CombatHintsGroup { get => _combatHintsGroup; }
+
+        public void SetAttackPoint(Vector3 value) => _attackPoint = value;
+
+        public void SetSpeed(float value) => NavMeshAgent.speed = value;
+
+        public override void OnUpdate()
+        {
+            _weapon.SetFireTarget(_attackPoint);
+            ManageAnimate();
+        }
+
+        private void ManageAnimate()
+        {
+            Vector3 dir;
+            if (FaceTarget)
+            {
+                dir = AttackPoint - Head.position;
+                dir.Normalize();
+                dir = transform.InverseTransformDirection(dir);
+            }
+            else
+            {
+                dir = Vector3.back;
+            }
+            Soldier.SetFloat("dir_x", dir.x, 0.1f, Time.deltaTime);
+            Soldier.SetFloat("dir_y", dir.z, 0.1f, Time.deltaTime);
+            Soldier.SetLayerWeight(1, Animator.velocity.magnitude / 10.7f);
+        }
+
+        public override void UpdateMovement()
+        {
+            Vector3 worldDelta = NavMeshAgent.nextPosition - transform.position;
+            worldDelta.y = 0;
+
+            float dx = Vector3.Dot(transform.right, worldDelta);
+            float dy = Vector3.Dot(transform.forward, worldDelta);
+            Vector2 deltaPos = new Vector2(dx, dy);
+            float smooth = Mathf.Min(1, Time.deltaTime / 0.1f);
+            _smoothDeltaPos = Vector2.Lerp(_smoothDeltaPos, deltaPos, smooth);
+            _velocity = _smoothDeltaPos / Time.deltaTime;
+            float turn = Vector3.Dot(transform.right, (transform.position - NavMeshAgent.nextPosition).normalized);
+            float steeringDot = Vector3.Dot((NavMeshAgent.steeringTarget - transform.position).normalized, transform.right.normalized);
+
+            if (NavMeshAgent.remainingDistance <= NavMeshAgent.stoppingDistance)
+            {
+                _velocity = Vector2.Lerp(Vector2.zero, _velocity, NavMeshAgent.remainingDistance / NavMeshAgent.stoppingDistance);
+            }
+            _maxspeed = Mathf.Clamp(NavMeshAgent.remainingDistance, 2f, 10.7f);
+            _velocity = _velocity.normalized * Mathf.Lerp(7, 1, Mathf.Abs(turn));
+            if (steeringDot < 0.25f)
+            {
+                Animator.SetFloat("vel_forward", _velocity.y, 0.1f, Time.deltaTime);
+            }
+            else
+            {
+                Animator.SetFloat("vel_forward", 1, 0.1f, Time.deltaTime);
+            }
+            Animator.SetFloat("turn", turn, .15f, Time.deltaTime);
+        }
+
+        internal void AllowFire(bool v)
+        {
+            if (IsDead) return;
+            _weapon.AllowFire = v;
+        }
+    }
+
+    public class HorsePatrolActBusyState : BaseState
+    {
+        private ChivalryAgentController _horse;
+
+        public HorsePatrolActBusyState(AgentController context) : base(context)
+        {
+            _horse = context as ChivalryAgentController;
+        }
+
+        public override void DrawGizmos()
+        {
+        }
+
+        public override void End()
+        {
+        }
+
+        public override void Start()
+        {
+        }
+
+        public override void Think()
+        {
+        }
+    }
+
+    public class HorseApproachCombatState : BaseState
+    {
+        private ChivalryAgentController _horse;
+
+        public HorseApproachCombatState(AgentController context) : base(context)
+        {
+            _horse = context as ChivalryAgentController;
+        }
+
+        public bool Completed => _completed;
+
+        public override void DrawGizmos()
+        {
+            if (_travel != null)
+            {
+                foreach (Vector3 child in _travel)
+                {
+                    Gizmos.color = Color.blue;
+                    Gizmos.DrawCube(child, Vector3.one * 1f);
+                }
+            }
+        }
+
+        public override void End()
+        {
+        }
+
+        public override void Start()
+        {
+            _horse.Animator.SetTrigger("ALERT");
+            index = 0;
+            _completed = false;
+            _hasTravel = false;
+            CreateVectors();
+        }
+
+        private Vector3 _startVector = Vector3.zero;
+
+        private bool _hasTravel;
+        private bool _completed;
+
+        //that can't raycast to either npc nor target, but can raycast to vector A and B
+        private Vector3 _middleVector = Vector3.zero;
+
+        private void CreateVectors()
+        {
+            Vector3 start = AgentSpatialUtility.GetBestPoint(AgentSpatialUtility.CreateCoverArray(new Vector2Int(30, 30), _horse.transform.position, _horse.PlayerHeadPosition, _horse.CoverMask)).Value.Position;
+            HorseNavHint startHint = _horse.CombatHintsGroup.NearestHintFromPoint(start);
+            Vector3 attack = AgentSpatialUtility.GetBestPoint(AgentSpatialUtility.CreateAttackArray(new Vector2Int(30, 30), _horse.PlayerGameObject.transform.position, _horse.PlayerHeadPosition, _horse.CoverMask)).Value.Position;
+            HorseNavHint endHint = _horse.CombatHintsGroup.NearestHintFromPoint(attack);
+            _horse.SetTarget(startHint.position);
+            index = 0;
+
+            HorseNavHint[] hints = _horse.CombatHintsGroup.FindPath(startHint, endHint);
+
+            if (hints.Length > 0)
+            {
+                _travel = new Vector3[hints.Length];
+                for (int i = 0; i < hints.Length; i++)
+                {
+                    _travel[i] = hints[i].position;
+                }
+                _hasTravel = true;
+            }
+        }
+
+        public override void Think()
+        {
+            ManageApproach();
+            ManageShooting();
+        }
+
+        private Vector3[] _travel;
+        private int index = 0;
+
+        private void ManageApproach()
+        {
+            if (!_hasTravel) return;
+
+            if (Vector3.Distance(_horse.transform.position, _travel[index]) < 5)
+            {
+                if (index < _travel.Length - 1)
+                {
+                    index++;
+                    _horse.SetTarget(_travel[index]);
+                }
+                else
+                {
+                    _hasTravel = false;
+                    _completed = true;
+                }
+            }
+        }
+
+        private void ManageShooting()
+        {
+            Vector3 dir = Vector3.zero;
+
+            if (_horse.HasPlayerVisual)
+            {
+                _horse.AllowFire(true);
+                _horse.SetAttackPoint(_horse.PlayerHeadPosition);
+                _horse.FaceTarget = true;
+            }
+            else
+            {
+                _horse.FaceTarget = false;
+                _horse.AllowFire(false);
+            }
+        }
+    }
+
+    public class HorseRetreatCombatState : BaseState
+    {
+        private ChivalryAgentController _horse;
+        private int index;
+        private Vector3[] _travel;
+        private bool _hasTravel;
+        private bool _completed;
+        public bool Completed => _completed;
+
+        public HorseRetreatCombatState(AgentController context) : base(context)
+        {
+            _horse = context as ChivalryAgentController;
+        }
+
+        public override void DrawGizmos()
+        {
+        }
+
+        public override void End()
+        {
+        }
+
+        public override void Start()
+        {
+            _horse.Animator.SetTrigger("ALERT");
+            _hasTravel = false;
+            _completed = false;
+            CreateVectors();
+        }
+
+        private void CreateVectors()
+        {
+            SpatialData[] spatialData = AgentSpatialUtility.CreateAttackArray(new Vector2Int(30, 30), _horse.PlayerGameObject.transform.position, _horse.PlayerHeadPosition, _horse.CoverMask).ToArray();
+
+            Vector3 escapePoint = spatialData[spatialData.Length - 1].Position;
+            HorseNavHint[] hintsDistance = _horse.CombatHintsGroup.Hints;
+            Array.Sort(hintsDistance, _horse.CombatHintsGroup.CompareDistance);
+
+            HorseNavHint endHint = hintsDistance.Reverse().ToArray()[0];
+
+            HorseNavHint startHint = _horse.CombatHintsGroup.NearestHintFromPoint(_horse.transform.position);
+
+            _horse.SetTarget(startHint.position);
+            index = 0;
+
+            HorseNavHint[] path = _horse.CombatHintsGroup.FindPath(startHint, endHint);
+
+            if (path.Length > 0)
+            {
+                _travel = new Vector3[path.Length];
+                for (int i = 0; i < path.Length; i++)
+                {
+                    _travel[i] = path[i].position;
+                }
+                _hasTravel = true;
+            }
+        }
+
+        private void ManageApproach()
+        {
+            if (!_hasTravel) return;
+
+            if (Vector3.Distance(_horse.transform.position, _travel[index]) < 5)
+            {
+                if (index < _travel.Length - 1)
+                {
+                    index++;
+                    _horse.SetTarget(_travel[index]);
+                }
+                else
+                {
+                    _hasTravel = false;
+                    _completed = true;
+                }
+            }
+        }
+
+        public override void Think()
+        {
+            ManageApproach();
+            ManageShooting();
+        }
+
+        private void ManageShooting()
+        {
+            Vector3 dir = Vector3.zero;
+
+            if (_horse.HasPlayerVisual)
+            {
+                _horse.AllowFire(true);
+                _horse.SetAttackPoint(_horse.PlayerHeadPosition);
+
+                _horse.FaceTarget = true;
+            }
+            else
+            {
+                _horse.FaceTarget = false;
+                _horse.AllowFire(false);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Game/Life/Entities/HorseNavHintGroup.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Game.Life.WaypointPath;
+using UnityEditor;
+using UnityEngine;
+
+namespace Life.Entities
+{
+    internal enum HorseNavHitType
+    {
+        COMBAT,
+        IDLE,
+        EXIT
+    }
+
+    public class HorseNavHint
+    {
+        private HorseNavHint[] _neightbours;
+        public Vector3 position;
+
+        public HorseNavHint[] Neightbours { get => _neightbours; }
+
+        public void SetNeightbours(HorseNavHint[] neightbours)
+        {
+            _neightbours = neightbours;
+        }
+    }
+
+    public class HorseNavHintGroup : MonoBehaviour
+    {
+        [SerializeField] private HorseNavHitType _type;
+        [SerializeField] private float _mergeDistance = 5;
+        private Transform[] _combatHints;
+
+        private HorseNavHint[] _hints;
+
+        private void Start()
+        {
+            _combatHints = transform.Cast<Transform>().ToArray();
+            BuildHints();
+        }
+
+        [ContextMenu("Build")]
+        private void BuildHints()
+        {
+            _hints = new HorseNavHint[_combatHints.Length];
+
+            for (int i = 0; i < _hints.Length; i++)
+            {
+                _hints[i] = new HorseNavHint();
+                _hints[i].position = _combatHints[i].position;
+            }
+            //build neightbours
+            for (int i = 0; i < _hints.Length; i++)
+            {
+                List<HorseNavHint> neightbours = new List<HorseNavHint>();
+                //add by distance
+                foreach (HorseNavHint n in _hints)
+                {
+                    if (n == _hints[i]) { continue; }
+
+                    if (Vector3.Distance(n.position, _hints[i].position) < _mergeDistance)
+                    {
+                        neightbours.Add(n);
+                    }
+                }
+                _hints[i].SetNeightbours(neightbours.ToArray());
+            }
+        }
+
+        public Transform NearestTransformFromPoint(Vector3 point)
+        {
+            pos = point;
+            Transform[] hints = _combatHints;
+            Array.Sort(hints, CompareDistance);
+            return hints[0];
+        }
+
+        public HorseNavHint NearestHintFromPoint(Vector3 point)
+        {
+            pos = point;
+            HorseNavHint[] hints = _hints;
+            Array.Sort(hints, CompareDistance);
+            return hints[0];
+        }
+
+        public HorseNavHint[] FindPath(HorseNavHint from, HorseNavHint to)
+        {
+            if (from == to)
+            {
+                Debug.LogError("target and origin are the same PELOTUDAZO");
+                return null;
+            }
+            foreach (HorseNavHint hint in from.Neightbours)
+            {
+                if (to == hint)
+                {
+                    Debug.LogWarning("path is too short!");
+                    return new HorseNavHint[] { from, to };
+                }
+            }
+            return SolvePath(from, to);
+        }
+
+        private HorseNavHint[] SolvePath(HorseNavHint from, HorseNavHint to)
+        {
+            Queue<HorseNavHint> BFSQueue = new();
+            Dictionary<HorseNavHint, HorseNavHint> visited = new Dictionary<HorseNavHint, HorseNavHint>();
+            BFSQueue.Enqueue(from);
+            HorseNavHint[] previous = new HorseNavHint[_hints.Length];
+            visited.Add(from, null);
+            bool pathFound = false;
+
+            while (BFSQueue.Count > 0)
+            {
+                HorseNavHint h = BFSQueue.Dequeue();
+                HorseNavHint[] neightbours = h.Neightbours;
+
+                if (h == to)
+                {
+                    //found it
+                    pathFound = true;
+                    break;
+                }
+
+                for (int i = 0; i < neightbours.Length; i++)
+                {
+                    if (!visited.ContainsKey(neightbours[i]))
+                    {
+                        visited.Add(neightbours[i], h);
+                        BFSQueue.Enqueue(neightbours[i]);
+                        previous[i] = h;
+                    }
+                }
+            }
+            if (pathFound)
+            {
+                List<HorseNavHint> path = new List<HorseNavHint>();
+                HorseNavHint current = to;
+
+                while (current != null)
+                {
+                    path.Add(current);
+                    current = visited[current];
+                }
+
+                path.Reverse(); // Reverse to get start->end order
+                return path.ToArray();
+            }
+            else return null;
+        }
+
+      
+        public Transform[] CombatHints => _combatHints;
+
+        private Vector3 pos;
+        public HorseNavHint[] Hints { get => _hints; }
+
+        public int CompareDistance(Transform hintA, Transform hintB)
+        {
+            return Vector3.Distance(hintA.transform.position, pos).CompareTo(Vector3.Distance(hintB.transform.position, pos));
+        }
+
+        public int CompareDistance(HorseNavHint hintA, HorseNavHint hintB)
+        {
+            return Vector3.Distance(hintA.position, pos).CompareTo(Vector3.Distance(hintB.position, pos));
+        }
+
+        [SerializeField] private Transform _endTest;
+        [SerializeField] private Transform _startTest;
+        [SerializeField] private bool _debugPath;
+
+        private void OnDrawGizmos()
+        {
+         
+            if (!Application.isPlaying)
+            {
+                Gizmos.color = Color.red;
+                List<Transform> children = transform.Cast<Transform>().ToList();
+
+                foreach (Transform child in children)
+                {
+                    foreach (Transform childx in children)
+                    {
+                        if (childx == child) { continue; }
+                        if (Vector3.Distance(childx.position, child.position) < _mergeDistance)
+                        {
+                            Gizmos.DrawLine(childx.position, child.position);
+                        }
+                    }
+                    Gizmos.DrawCube(child.position, Vector3.one * 0.25f);
+                }
+                return;
+            }
+
+            foreach (HorseNavHint child in _hints)
+            {
+                Gizmos.color = Color.blue;
+                Gizmos.DrawCube(child.position, Vector3.one * 0.25f);
+
+                foreach (HorseNavHint n in child.Neightbours)
+                {
+                    Gizmos.DrawLine(child.position, n.position);
+                }
+            }
+        }
+    }
+}
